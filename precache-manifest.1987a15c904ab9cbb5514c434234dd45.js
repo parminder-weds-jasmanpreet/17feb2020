f71bd76feacb59df49f83e97e5355af95fd719fd
gurpreet singh <birdi.gpsingh@gmail.com>
2020-02-03 12:13:59 +0530

    Updates

---
+++ precache-manifest.1987a15c904ab9cbb5514c434234dd45.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1a3f74c9dc04a9d68a6c60fa376dc39",
+    "url": "/17feb2020/index.html"
+  },
+  {
+    "revision": "28b34dc05aee618a6920",
+    "url": "/17feb2020/static/js/2.cd542992.chunk.js"
+  },
+  {
+    "revision": "3a91fcaafb25a652e216",
+    "url": "/17feb2020/static/js/main.abeade99.chunk.js"
+  },
+  {
+    "revision": "43c6f373941f988f64dc",
+    "url": "/17feb2020/static/js/runtime-main.f31dab96.js"
+  }
+]);
